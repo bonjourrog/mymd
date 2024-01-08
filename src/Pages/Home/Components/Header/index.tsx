1a8e95c93060b550711f7f25ae5371bf64ccaac8
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-01-07 19:19:50 -0700

    feat: create Header component

---
+++ src/Pages/Home/Components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+import { HeaderProps } from './Header.props';
+
+const Header: React.FC<HeaderProps> = ()=>{
+    return(
+        <header className='header'></header>
+    );
+}
+
+
+export default Header;
