b6e1a996c7f1e4e3cd3e36f0724c1b181baea7c7
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-01-09 12:17:19 -0700

    feat: add and style input text to name the file and download button

--- src/Pages/Home/Components/Header/index.tsx
+++ src/Pages/Home/Components/Header/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { HeaderProps } from './Header.props';
+import { IoCloudDownloadSharp } from "react-icons/io5";
 
 const Header: React.FC<HeaderProps> = ()=>{
+    const [noteTitle, setNoteTitle]= useState<string>("New file");
     return(
-        <header className='header'></header>
+        <header className='header'>
+            <input className='header__note-title' value={noteTitle} onChange={(e)=>setNoteTitle(e.target.value)} onFocus={(e)=>e.currentTarget.select()} type='text' placeholder='Ingrese el nombre del archivo ✍🏻🖊️✏️✨'/>
+
+            <ul className='flex gap-8 items-center'>
+                <li>
+                    <IoCloudDownloadSharp className="header__download-btn" />
+                </li>
+            </ul>
+        </header>
     );
 }
 
